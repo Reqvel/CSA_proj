95b821d8903d063dde4c1769935caf127aea08c4
Sergey <sergey.sh.hodakovs@gmail.com>
2021-11-24 14:57:13 +0300

    insert data func

--- db.py
+++ db.py
@@ -10,3 +10,10 @@ def create_locations_table(sql_connection, table_name):
         )
     """)
     sql_connection.commit()
+
+
+def insert_data_csv(sql_connection, table_name, column, arr_i, generator_csv):
+    cursor = sql_connection.cursor()
+    for arr in generator_csv:
+        cursor.execute(f"INSERT INTO {table_name} ({column}) VALUES (?)", (arr[arr_i],))
+    sql_connection.commit()
