bfd9a125b82d3cd2a4de7955cce317b1e829413b
Sergey <sergey.sh.hodakovs@gmail.com>
2021-11-26 20:40:15 +0300

    async get location center

--- mapbx.py
+++ mapbx.py
@@ -1,3 +1,5 @@
+import asyncio
+import aiohttp
 import pip._vendor.requests as requests
 import json
 
@@ -16,26 +18,19 @@ class MapBx:
         self._mapbox_access_token = json_object[self.access_token_key]
 
 
-    def get_location_center(self, location_name: str, info: bool) -> tuple:
+    async def get_location_center(self, session, location_name: str) -> tuple:
         url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{location_name}.json?country=by&access_token={self._mapbox_access_token}"
-        json_data = requests.get(url).json()
-        if(json_data and json_data['features']):
-            for res in json_data['features']:
-                if(res['text'] == location_name):
-                    if(info):
-                        print(f"place_name : {res['place_name']}")
-                        print(f"text : {res['text']}")
-                        print(f"center : {res['center']}")
-                        print('\n\n')
-                    longitude = res['center'][0]
-                    latitude = res['center'][1]
-                    return longitude, latitude
-        else:
-            if(info):
-                print("*** DATA IS NULL ***")
-                print('\n\n')
-        return None, None
+        
+        async with session.get(url) as response:
+            json_data = await response.json()
+            if(json_data and json_data['features']):
+                for res in json_data['features']:
+                    if(res['text'] == location_name):
+                        longitude = res['center'][0]
+                        latitude = res['center'][1]
+                        return longitude, latitude
+            return None, None
 
-    
+
     def get_token(self):
         return self._mapbox_access_token
