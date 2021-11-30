f1929864ae0e5957d8fa515da315ade631875e9b
Sergey <sergey.sh.hodakovs@gmail.com>
2021-11-30 09:04:08 +0300

    initial clustering update

--- cluster.py
+++ cluster.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from pandas.core.frame import DataFrame
 
 class Cluster:
@@ -9,16 +8,23 @@ class Cluster:
             if not in_cluster[point]:
                 near_points = durations.loc[
                     (durations['a_location_id'] == point) & (durations['duration_hours'] <= duration)]
-                nearest_point = None
-                for _, row in near_points.iterrows():
-                    if not in_cluster[row['b_location_id']]:
-                        nearest_point = int(row['b_location_id'])
-                        break
-                if nearest_point:
+                if not near_points.empty:
+                    nearest_point = None
+                    for _, row in near_points.iterrows():
+                        if not in_cluster[row['b_location_id']]:
+                            nearest_point = int(row['b_location_id'])
+                            break
+                    if nearest_point:
+                        cluster = [point]
+                        cluster.append(nearest_point)
+                        clusters.append(cluster)
+                        in_cluster[point] = True
+                        in_cluster[nearest_point] = True
+                else:
                     cluster = [point]
-                    cluster.append(nearest_point)
                     clusters.append(cluster)
                     in_cluster[point] = True
-                    in_cluster[nearest_point] = True
+        
+        
             
         return clusters
