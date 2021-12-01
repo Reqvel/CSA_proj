0d26b4c1485d678cba2712d6ef801ec69ab24c4a
Sergey <sergey.sh.hodakovs@gmail.com>
2021-12-01 21:53:16 +0300

    clustering improvement

--- cluster.py
+++ cluster.py
@@ -11,55 +11,57 @@ class Cluster:
                     near_points = durations.loc[
                         (durations['a_location_id'] == point) & (durations['duration_hours'] <= duration)]
                     if not near_points.empty:
-                        nearest_point = None
+                        pair_point = None
                         for _, row in near_points.iterrows():
-                            if not in_cluster[row['b_location_id']]:
-                                nearest_point = int(row['b_location_id'])
+                            if not in_cluster[row['b_location_id']] and row['duration_hours'] != 0:
+                                pair_point = int(row['b_location_id'])
                                 break
-                        if nearest_point:
-                            cluster = [point, nearest_point]
+                        if pair_point:
+                            cluster = [point, pair_point]
                             in_cluster[point] = True
-                            in_cluster[nearest_point] = True
+                            in_cluster[pair_point] = True
                             return cluster
             
             return None
 
+        cluster_i = 0
         clusters.append(get_pair())
-        i = 0
-        cluster_filled = {i: False}
-        added = True
+        point_added = True
         cluster_added = False
+        cluster_filled = {cluster_i: False}
 
-        while added or cluster_added:
-            added = False
+        while point_added or cluster_added:
+            point_added = False
             cluster_added = False
-            for j, cluster in enumerate(clusters):
-                if not cluster_filled[j]:
+            for i, cluster in enumerate(clusters):
+                if not cluster_filled[i]:
                     near_points = []
                     for point in cluster:
                         near_points_df = durations.loc[
                         (durations['a_location_id'] == point) & (durations['duration_hours'] <= duration)]
                         near_points.append(set(near_points_df['b_location_id'].tolist()))
                     points_intersection = None
-                    for i_points in near_points:
+                    for iter_points in near_points:
                         if points_intersection == None:
-                            points_intersection = i_points
-                        points_intersection &= i_points
-                    for point in points_intersection:
-                        if not in_cluster[point]:
-                            cluster.append(point)
-                            in_cluster[point] = True
-                            added = True
+                            points_intersection = iter_points
+                            continue
+                        points_intersection &= iter_points
+                    if points_intersection:
+                        for point in points_intersection:
+                            if not in_cluster[point]:
+                                cluster.append(point)
+                                in_cluster[point] = True
+                                point_added = True
 
-                    if not added:
-                        cluster_filled[i] = True
-                        i += 1
-                        cluster_filled[i] = False
+                    if not point_added:
+                        cluster_filled[cluster_i] = True
                 
-            if not added:
+            if not point_added:
                 new_cluster = get_pair()
                 if new_cluster:
                     clusters.append(new_cluster)
                     cluster_added = True
+                    cluster_i += 1
+                    cluster_filled[cluster_i] = False
             
         return clusters
