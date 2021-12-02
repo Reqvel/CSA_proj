6eeafcf684b0ca6b238a75ca39884185e6c30a33
Sergey <sergey.sh.hodakovs@gmail.com>
2021-12-02 18:52:11 +0300

    main file clean up

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from db import DB
 from mapbx import MapBx
 from visuals import *
 import csv
-import requests
 import asyncio
+from cluster import Cluster
+import time
 
 def get_generator_csv(file_name, delimiter, skip_rows):
     with open(file_name, 'r', encoding="UTF8", errors='ignore') as csv_file:
@@ -25,56 +26,39 @@ def main():
 
     db.connect()
 
-    if(fresh_init):
+    if(fresh_init): # CHANGE ASYNC TO SYNC 
         db.create_locations_table(locations_table_name)
         generator_csv = get_generator_csv('csvs/place_name_raw_data.csv', '\t', 1)
         db.insert_data_csv(locations_table_name, 'name', 9, generator_csv)
         generator_csv = get_generator_csv('csvs/population_by_0.csv', ',', 2)
         db.update_population_by_name_csv(locations_table_name, generator_csv)
-        asyncio.run(db.update_location_center(locations_table_name, mpbx.get_location_center))
+        asyncio.run(db.update_location_center_async(locations_table_name, mpbx.get_location_center_async))
 
-        # db.create_durations_table(durations_table_name)
-        # locations = db.select_all_with_conditions(locations_table_name)
-        # asyncio.run(db.init_durations_table(durations_table_name, locations, mpbx.get_duration))
+        db.create_durations_table(durations_table_name)
+        locations = db.select_all_with_conditions(locations_table_name)
+        asyncio.run(db.init_durations_table_async(durations_table_name, locations, mpbx.get_duration_async))
 
-    
-    # db.create_durations_table(durations_table_name)
-    locations = db.select_all_with_conditions(locations_table_name)
-    asyncio.run(db.init_durations_table(durations_table_name, locations, mpbx.get_duration))
+    c = Cluster()
+    curs = db.select_locations_ids_with_conditions(locations_table_name)
+    points = [point[0] for point in curs]
+    durations = db.get_durations_pandas_df(durations_table_name)
+    duration_list = [0.5, 1, 1,5, 3, 4]
+    for duration in duration_list:
+        start = time.time()
+        df = c.cluster_by_duration(points, durations, duration, locations_table_name, db.get_location_info_by_id)
+        show_locations(df, mpbx.get_token(), duration)
+        end = time.time()
+        print(f'Whitin {duration} Hours. Time Taken To Build: {end - start} sec.')
 
-    dur = db.select_all(durations_table_name).fetchall()
-    print(dur)
-    print(len(dur))
+    # FIND INFO ABOUT LOCATION
+    # df_loc = db.get_locations_pandas_df(locations_table_name)
+    # df = df_loc.loc[df_loc['name'] == 'Ганцевичи']
+    # print(df)
 
-
-    # locations = db.select_all_with_conditions(locations_table_name)
-    # asyncio.run(db.init_durations_table(durations_table_name, locations, mpbx.get_duration))
-
-    # data_frame = db.get_pandas_df(locations_table_name)
-    # show_locations(data_frame, mpbx.get_token())
-
-    # db.disconnect()
-
-
-    # cursor = sql_connection.cursor()
-    # cursor.execute(f"""
-    #     SELECT DISTINCT name, longitude, latitude, population
-    #     FROM {locations_table_name} 
-    #     WHERE name = 'Берёза'""")
-    # print(cursor.fetchall())
-
-    # with open('private.json', 'r') as openfile:
-    #     json_object = json.load(openfile)
-    # mapbox_access_token = json_object["mapbox_access_token"]
-    # locations_list = db.select_all_with_conditions(locations_table_name).fetchall()
-    # a_lon = locations_list[0][2]
-    # a_lat = locations_list[0][3]
-    # b_lon = locations_list[1][2]
-    # b_lat = locations_list[1][3]
-    # url = f"https://api.mapbox.com/optimized-trips/v1/mapbox/driving/{a_lon},{a_lat};{b_lon},{b_lat}?access_token={mpbx.get_token()}"
-    # json_data = requests.get(url).json()
-    # if(json_data):
-    #     print(json_data['trips'][0]['duration'])
+    # FIND DURATION BETWEEN LOCATIONS
+    # dfdr = db.get_durations_pandas_df(durations_table_name)
+    # df = dfdr.loc[(dfdr['a_location_id'] == 1) & (dfdr['b_location_id'] == 991)]
+    # print(df)
 
     db.disconnect()
 
